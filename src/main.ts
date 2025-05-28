287405558cbccc6b05345f6fcd37d916d5929a06
Dimitar Ivanov <quadcorebg@gmail.com>
2025-05-29 01:38:33 +0300

    feat: add configuration and validation for environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { appCreate } from './app.create';
 
@@ -8,7 +9,10 @@ async function bootstrap() {
   // All necessary middleware and global pipes can be set up in app.create.ts
   appCreate(app);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('appConfig.port') || 3000;
+
+  await app.listen(port);
 }
 
 bootstrap();
