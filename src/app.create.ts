ac4c7b67f7019f82d83f2909ca1102f53ed8e625
Dimitar Ivanov <quadcorebg@gmail.com>
2025-05-29 01:08:16 +0300

    feat: add Swagger support for API documentation

--- src/app.create.ts
+++ src/app.create.ts
@@ -1,5 +1,16 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { INestApplication } from '@nestjs/common';
 
 export function appCreate(app: INestApplication): void {
   app.enableCors();
+
+  // Swagger configuration; To be configured later
+  const swaggerConfig = new DocumentBuilder()
+    .addServer('http://localhost:3000')
+    .setVersion('1.0')
+    .build();
+
+  // Instantiate Document
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 }
